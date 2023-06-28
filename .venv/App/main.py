3bc20dd069b8e90e14ebd7c3466959b46707ffc0
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-06-29 00:12:50 +0530

    Routing is Completed

--- .venv/App/main.py
+++ .venv/App/main.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello FLask I Started Working on that!</p>"
 
+@app.route("/hi")
+def Hi():
+    return "<h1>Hello FLask</h1>"
 if __name__ == "__main__":
     app.run()
